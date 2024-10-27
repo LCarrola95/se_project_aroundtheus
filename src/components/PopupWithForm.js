5da0fd596bba8a58d01536561a1c14af2b397c73
Luis Carrola <lcarrola70@gmail.com>
2024-10-27 14:00:01 -0600

    PopupWithForm line 26 edit

--- src/components/PopupWithForm.js
+++ src/components/PopupWithForm.js
@@ -23,7 +23,6 @@ export default class PopupWithForm extends Popup {
     this._formElement.addEventListener("submit", (e) => {
       e.preventDefault();
       this._handleFormSubmit(this._getInputValues());
-      this.resetForm();
     });
   }
 
